a22dc62b8fed0c811a2b7f017d07e4d7358d1fe4
rango <jylongoo@163.com>
2025-10-17 16:51:54 +0800

    Initial Commit

---
+++ PhotoBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoBox
+//
+//  Created by rango on 2025/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoBox/PhotoBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBoxApp.swift
+//  PhotoBox
+//
+//  Created by rango on 2025/10/17.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
